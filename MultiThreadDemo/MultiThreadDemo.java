f0f56ab5e1f0cd837cbd34a730c75fdecb73854c
Sachin Maurya <kumarsachin67671@gmail.com>
2025-03-25 23:00:44 +0530

    1 multithreading

---
+++ MultiThreadDemo/MultiThreadDemo.java
@@ -0,0 +1,33 @@
+package MultiThreadDemo;
+
+class MultiThreadDemo implements Runnable {
+    private String threadName;
+
+    MultiThreadDemo(String name) {
+        threadName = name;
+    }
+
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(threadName + " - Count: " + i);
+            try {
+                // Sleep for a while to simulate some work and allow context switching
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(threadName + " interrupted.");
+            }
+        }
+        System.out.println(threadName + " exiting.");
+    }
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new MultiThreadDemo("Thread 1"));
+        Thread thread2 = new Thread(new MultiThreadDemo("Thread 2"));
+        Thread thread3 = new Thread(new MultiThreadDemo("Thread 3"));
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
+
